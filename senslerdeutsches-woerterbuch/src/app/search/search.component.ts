b504dba92cc97447bbda43cd1e82a6934296589d
nw <nicolas.wenk@softcom.pro>
2025-03-06 15:36:39 +0100

    Binds search with ngModel

--- senslerdeutsches-woerterbuch/src/app/search/search.component.ts
+++ senslerdeutsches-woerterbuch/src/app/search/search.component.ts
@@ -1,28 +1,46 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, DestroyRef, EventEmitter, Output } from '@angular/core';
 import { SearchService } from '../services/search.service';
-import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
-import { Router, RouterModule } from '@angular/router';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss',
 })
 export class SearchComponent {
-  constructor(private readonly searchService: SearchService, private readonly router: Router) {}
 
-  protected searchResults: any = undefined;
+  private _searchTerm: string = '';
 
-  onSearchPreview(event: Event) {
-    const inputElement = event.target as HTMLInputElement;
-    const query = inputElement.value;
+  public get searchTerm(): string {
+    return this._searchTerm;
+  }
 
-    this.searchService.autoComplete(query).subscribe((data) => {
+  public set searchTerm(value: string) {
+    this.searchService.autoComplete(value).subscribe((data) => {
       this.searchResults = data;
     });
+    this._searchTerm = value;
   }
+
+  constructor(
+    private readonly searchService: SearchService,
+    private readonly router: Router,
+    readonly destroyRef: DestroyRef) {
+
+    this.router.events.pipe(takeUntilDestroyed(destroyRef)).subscribe((event: any) => {
+      if(event instanceof NavigationEnd) {
+
+        let urlSegments = (event as NavigationEnd).url.split('/');
+        this.searchTerm = urlSegments[urlSegments.length-1];
+      }
+    });
+  }
+
+  protected searchResults: any = undefined;
 
   onSearch(event: Event){
     const inputElement = event.target as HTMLInputElement;
