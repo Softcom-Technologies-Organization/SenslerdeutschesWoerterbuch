7028db2763a04a5e03b92a2456a65653378a506c
Gregor Boschung <gregor.boschung@softcom.pro>
2025-03-10 13:22:35 +0100

    improve styling

--- senslerdeutsches-woerterbuch/src/app/app.component.ts
+++ senslerdeutsches-woerterbuch/src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { MatDivider } from '@angular/material/divider';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatCard, MatCardContent } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, MatDivider],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    MatDivider,
+    MatToolbar,
+    MatCard,
+    MatCardContent,
+    RouterModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
