034621d84107ab42e825e08eb310d485702bdb5b
GregorBoschungSoftcom <gregor.boschung@softcom.pro>
2025-04-06 23:27:45 +0200

    Remove unused import from search service

--- frontend/src/app/services/search.service.ts
+++ frontend/src/app/services/search.service.ts
@@ -2,7 +2,6 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
-import { F } from '@angular/cdk/keycodes';
 
 @Injectable({
   providedIn: 'root',
