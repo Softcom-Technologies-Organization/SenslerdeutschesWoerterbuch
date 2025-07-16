6943da990d52dd9719ea2cb9f8add282cd94de5b
Frozzie89 <froztor85@gmail.com>
2025-07-16 16:33:08 +0200

    display tags in word result list

--- frontend/src/app/search/search.component.ts
+++ frontend/src/app/search/search.component.ts
@@ -1,7 +1,7 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SearchResult, SearchService } from '../services/search.service';
 import { CommonModule } from '@angular/common';
-import { Router, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -13,8 +13,7 @@ import { MatIcon } from '@angular/material/icon';
 import { TagTranslationPipe } from "../pipes/tag-translation.pipe";
 import { MatSelect } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
-import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-search',
@@ -52,21 +51,7 @@ export class SearchComponent implements OnInit {
 
   private exactMatchMode = false;
 
-  // Dice icon from Font-Awesome, free icon.
-  diceIcon = 
-    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" fill="#6a4b46"><path d="M274.9 34.3c-28.1-28.1-73.7-28.1-101.8 0L34.3 173.1c-28.1 28.1-28.1 73.7 0 101.8L173.1 413.7c28.1 28.1 73.7 28.1 101.8 0L413.7 274.9c28.1-28.1 28.1-73.7 0-101.8L274.9 34.3zM200 224a24 24 0 1 1 48 0 24 24 0 1 1 -48 0zM96 200a24 24 0 1 1 0 48 24 24 0 1 1 0-48zM224 376a24 24 0 1 1 0-48 24 24 0 1 1 0 48zM352 200a24 24 0 1 1 0 48 24 24 0 1 1 0-48zM224 120a24 24 0 1 1 0-48 24 24 0 1 1 0 48zm96 328c0 35.3 28.7 64 64 64l192 0c35.3 0 64-28.7 64-64l0-192c0-35.3-28.7-64-64-64l-114.3 0c11.6 36 3.1 77-25.4 105.5L320 413.8l0 34.2zM480 328a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"/></svg>'
-
-  constructor(
-    readonly searchService: SearchService, 
-    private router : Router
-  ) {
-      const iconRegistry = inject(MatIconRegistry);
-      const sanitizer = inject(DomSanitizer);
-      iconRegistry.addSvgIconLiteral(
-        'dice',
-        sanitizer.bypassSecurityTrustHtml(this.diceIcon),
-    );
-  }
+  constructor(readonly searchService: SearchService) {}
 
   ngOnInit() {
     this.results$ = this.searchTermSubject.pipe(
